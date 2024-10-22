f933b54b9a208f101270a4bde995db4a97534553
Daniel <denial821124@gmail.com>
2024-10-22 11:23:25 +0800

    第一次推版

---
+++ TeaTimeDemo/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeaTimeDemo.Models
+{
+    public class Category
+    {
+        [Key] 
+        public int Id { get; set; }
+        [Required(ErrorMessage = "類別名稱 請記得輸入。")]
+        [MaxLength(30)]
+        [DisplayName("類別名稱")]
+        public string Name { get; set; }
+        [DisplayName("顯示順序")]
+        //[Range(0, 100)]
+        [Range(1, 100,ErrorMessage ="輸入範圍應該要在 1-100之間")]
+        public int DisplayOrder {  get; set; }
+    }
+}
